61e6f558d0879e6d40599140f6c521bf1733b5d8
junjinyong <junjinyong@snu.ac.kr>
2023-07-22 08:31:59 +0900

    Implemented face recognition

--- database.py
+++ database.py
@@ -9,28 +9,38 @@ import numpy as np
 
 
 class FaceDatabase:
-    def __init__(self, decay=0.1):
+    def __init__(self, threshold=0.5, decay=0.1):
         self.__data = list()
+        self.__blur = list()
         self.__size = 0
+        self.__threshold = threshold
         self.__decay = decay
 
-    def __get_distance(self, face):
-        return np.linalg.norm(self.__data - face, axis=1) if self.__size > 0 else np.empty(0)
-
-    def query(self, face):
+    def query(self, face, update=True, insert=True):
+        # Initialize minimum and index
         minimum = float("inf")
         index = -1
+
+        # Calculate minimum and index
         if self.__size > 0:
-            distances = self.__get_distance(face)
+            distances = np.linalg.norm(self.__data - face, axis=1)
             minimum = min(distances)
             index = np.argmin(distances)
-        return minimum, index
 
-    def insert(self, face):
-        self.__data.append(face)
-        size = self.__size
-        self.__size = size + 1
-        return size
+        # Flag indicates whether the face already exists
+        flag = minimum <= self.__threshold
 
-    def update(self, index, face):
-        self.__data[index] = self.__data[index] * (1.0 - self.__decay) + face * self.__decay
+        # Update face in the database
+        if update and flag:
+            self.__data[index] * (1.0 - self.__decay) + face * self.__decay
+
+        # Insert new face to the database
+        if insert and not flag:
+            self.__data.append(face)
+            index = self.__size
+            self.__size = index + 1
+
+        return index
+
+    def get_size(self):
+        return self.__size
--- final.py
+++ final.py
@@ -10,6 +10,7 @@
 import sys
 import cv2
 import numpy as np
+
 from recognizer import FaceRecognizer
 from blurer import Blurer
 from database import FaceDatabase
@@ -24,18 +25,24 @@ def onMouse(event, u, v, flags, param):
     pass
 
 
+def look(robot, camera, host):
+    # Get 2d coordinates
+    u = (host.left() + host.right()) / 2
+    v = (host.top() + host.bottom()) / 2
+
+    # Convert the 2d coordinates to 3d coordinates in camera frame
+    (x, y, z) = camera.convert2d_3d(u, v)
+    # Convert the 3d coordinates from the camera frame into
+    # Gretchen's frame using a transformatio matrix
+    (x, y, z) = camera.convert3d_3d(x, y, z)
+    # have Gretchen look at that point
+    robot.lookatpoint(x, y, z)
+
+
 def main():
-    # Set parameters
-    font = cv2.FONT_HERSHEY_DUPLEX
-    decay = 0.1
-    threshold = 0.5
-    upsamples = 1
-    jitters = 1
-
     # Initalize ROS environment
-    ROSEnvironment()
-
     # Initalize camera and robot
+    ROSEnvironment()
     camera = Camera()
     robot = Robot()
     camera.start()
@@ -44,7 +51,7 @@ def main():
     # Initialize face recognizer, face database and bluerer
     recognizer = FaceRecognizer()
     blurer = Blurer()
-    database = FaceDatabase(decay=decay)
+    database = FaceDatabase()
 
     # Create a window called "Frame" and install a mouse handler
     cv2.namedWindow("Frame")
@@ -56,23 +63,19 @@ def main():
         image = camera.getImage()
 
         # Detect faces from image
-        locations = recognizer.detect(image, upsample_num_times=upsamples)
+        locations = recognizer.detect(image, upsample_num_times=1)
 
         if locations:
             # Calculate face encoding
             landmarks = [recognizer.predict(image, location) for location in locations]
-            encodings = [np.array(recognizer.encode(image, landmark, num_jitters=jitters)) for landmark in landmarks]
+            encodings = [np.array(recognizer.encode(image, landmark, num_jitters=1)) for landmark in landmarks]
 
             indices = list()
             for face in encodings:
                 # Calculate the distance to all faces in the database
-                minimum, index = database.query(face)
-
                 # If there is a face whose distance is less than the threshold
                 # It is considered to belong to the same person
-                if minimum > threshold:
-                    index = database.insert(face)
-
+                index = database.query(face, update=True, insert=True)
                 indices.append(index)
 
             # Determine main person
@@ -87,20 +90,10 @@ def main():
                 left, top, right, bottom = face.left(), face.top(), face.right(), face.bottom()
                 name = "ID: " + str(index)
                 cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2)
-                cv2.putText(image, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+                cv2.putText(image, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 1.0, (255, 255, 255), 1)
 
             # Track the main person
-            # Get 2d coordinates
-            u = (host.left() + host.right()) / 2
-            v = (host.top() + host.bottom()) / 2
-
-            # Convert the 2d coordinates to 3d coordinates in camera frame
-            (x, y, z) = camera.convert2d_3d(u, v)
-            # Convert the 3d coordinates from the camera frame into
-            # Gretchen's frame using a transformatio matrix
-            (x, y, z) = camera.convert3d_3d(x, y, z)
-            # have Gretchen look at that point
-            robot.lookatpoint(x, y, z)
+            look(robot, camera, host)
 
         # Display image
         cv2.imshow("Frame", image[..., ::-1])
